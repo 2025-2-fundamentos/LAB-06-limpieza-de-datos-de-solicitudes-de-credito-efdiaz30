73eb2353c7f9add506f8fc1bf1d6010a51123914
EfrainDC <143148750+Efraindc@users.noreply.github.com>
2025-11-27 19:27:04 -0500

    prueba 1

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -2,7 +2,8 @@
 Escriba el codigo que ejecute la accion solicitada en la pregunta.
 """
 
-
+import pandas as pd
+import os
 def pregunta_01():
     """
     Realice la limpieza del archivo "files/input/solicitudes_de_credito.csv".
@@ -13,3 +14,36 @@ def pregunta_01():
     El archivo limpio debe escribirse en "files/output/solicitudes_de_credito.csv"
 
     """
+    file_path = 'files/input/solicitudes_de_credito.csv'
+    data = pd.read_csv(file_path, sep=';')
+
+    data.drop(['Unnamed: 0'], axis=1, inplace=True)
+    data.dropna(inplace=True)
+    data.drop_duplicates(inplace=True)
+
+    data[['día', 'mes', 'año']] = data['fecha_de_beneficio'].str.split('/', expand=True)
+    data.loc[data['año'].str.len() < 4, ['día', 'año']] = data.loc[data['año'].str.len() < 4, ['año', 'día']].values
+    data['fecha_de_beneficio'] = data['año'] + '-' + data['mes'] + '-' + data['día']
+    data.drop(['día', 'mes', 'año'], axis=1, inplace=True)
+
+    object_columns = ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'línea_credito']
+    data[object_columns] = data[object_columns].apply(lambda x: x.str.lower().replace(['-', '_'], ' ', regex=True).str.strip())
+    data['barrio'] = data['barrio'].str.lower().replace(['-', '_'], ' ', regex=True)
+
+    data['monto_del_credito'] = data['monto_del_credito'].str.replace("[$, ]", "", regex=True).str.strip()
+    data['monto_del_credito'] = pd.to_numeric(data['monto_del_credito'], errors='coerce')
+    data['monto_del_credito'] = data['monto_del_credito'].fillna(0).astype(int)
+    data['monto_del_credito'] = data['monto_del_credito'].astype(str).str.replace('.00', '')
+
+    data.drop_duplicates(inplace=True)
+
+    output_dir = 'files/output'
+    os.makedirs(output_dir, exist_ok=True)
+
+    output_path = f'{output_dir}/solicitudes_de_credito.csv'
+    data.to_csv(output_path, sep=';', index=False)
+
+    return data.head()
+
+if __name__ == '__main__':
+    pregunta_01()
